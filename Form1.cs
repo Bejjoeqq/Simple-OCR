688b95e9585f84691479ade9b741743f21fd02f8
Aldhiya Rozak <32904416+Bejjoeqq@users.noreply.github.com>
2020-07-21 15:34:45 +0700

    OCR

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+
+namespace Simple_OCR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Clipboard.GetImage();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(pictureBox1.Image);
+            TesseractEngine engine = new TesseractEngine("./tessdata", "eng", EngineMode.TesseractAndCube);
+            Page page = engine.Process(img,PageSegMode.Auto);
+            richTextBox1.Text = page.GetText();
+        }
+    }
+}
